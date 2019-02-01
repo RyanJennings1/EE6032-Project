6723b5cacaf32ca9300dca29da04940266c2cc90
Ryan Jennings <ryanjennings1010@gmail.com>
2019-02-01 22:20:09 +0000

    Update dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var mime = require('mime');
+var mime = require('mime-types');
 var cache = {};
 
 function send404(response) {
